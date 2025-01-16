c6e099c56c59d614b446f2c71faa53cb76f47f27
OscarBarrera89 <oscarbar745504@gmail.com>
2025-01-16 14:38:29 +0100

    se ve en odoo

--- models/veterinario.py
+++ models/veterinario.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class veterinario(models.Model):
     _inherit="quintopet.persona"
+    _name = "quintopet.veterinario"
     
     especialidad = fields.Char("Especialidad", size=64, required=True)
     numeroColegiado = fields.Integer("Numero de colegiado", default=25, required=True, readonly=False)
